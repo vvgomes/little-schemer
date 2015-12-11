f95b13f23520f88e157131c978f431557fabd707
Vinicius Gomes <vinicius.vieira.gomes@gmail.com>
2015-12-10 22:06:18 -0500

    attempt to fix the build 🙈

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject little-schemer "0.0.1-SNAPSHOT"
   :description "Cool new project to do things and stuff"
   :dependencies [[org.clojure/clojure "1.5.1"]]
-  :profiles {:dev {:dependencies [[midje "1.5.1"]]}})
+  :profiles {:dev {:dependencies [[midje "1.5.1"]]
+                   :plugins [[lein-midje "3.0.0"]]}})
   
